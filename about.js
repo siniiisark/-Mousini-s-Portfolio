b79cd8e6742b2de3c405e8f7b72d0fb033abf699
Mousini Sarkar <mousinisarkar05@gmail.com>
2024-12-17 14:14:38 +0530

    Update about.js

--- about.js
+++ about.js
@@ -13,7 +13,7 @@ function createStars() {
 
        
         const size = Math.random() * 3;
-        star.style.width = `${size}px`;
+         star.style.width = `${size}px`;
         star.style.height = `${size}px`;
 
        
